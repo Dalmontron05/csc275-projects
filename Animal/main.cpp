342728541d28d14b9faa32fe0e84a8e48e54af0f
Dalmontron05 <vexr2dts85338@gmail.com>
2024-09-11 13:04:17 -0700

    did animal polymorhism notes

---
+++ Animal/main.cpp
@@ -0,0 +1,70 @@
+//* polymorhism notes
+
+// Imports
+#include <iostream>
+using namespace std;
+
+
+// Functions
+class Animal
+{
+public:
+    // displays type of animal
+    void displayType()
+    {
+        cout << "I am an animal!" << endl;
+    }
+
+    void sound()
+    {
+        cout << "Animals make sounds!" << endl;
+    }
+};
+
+
+// Derived class - inherits from animal
+class Dog : public Animal
+{
+public:
+    void displayType()
+    {
+        cout << "I am a dog!" << endl;
+    }
+
+    void sound()
+    {
+        cout << "Woof! Woof!" << endl;
+    }
+};
+
+
+class Cat : public Animal
+{
+public:
+    void displayType()
+    {
+        cout << "I am a cat!" << endl;
+    }
+
+    void sound()
+    {
+        cout << "Meow" << endl;
+    }
+};
+
+
+int main()
+{
+    Animal animal1;
+    animal1.displayType();
+    animal1.sound();
+    
+    Dog dog1;
+    dog1.displayType();
+    dog1.sound();
+
+    Cat cat1;
+    cat1.displayType();
+    cat1.sound();
+}
+
